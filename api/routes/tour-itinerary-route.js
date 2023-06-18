21903ba0781eb43327426fc21d6bd9c940808dfc
ishwarsathe <ishwarsathe67@gmail.com>
2023-06-18 10:39:12 +0530

    Data added to DB fix

--- api/routes/tour-itinerary-route.js
+++ api/routes/tour-itinerary-route.js
@@ -3,6 +3,17 @@ import { createitinerary, deleteIntinerary, getAllIntineraryTour, getIntinerary,
 import { createItineraryValidation } from "../Schema/tour-itinerary-schema.js";
 const router = express.Router();
 
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, "uploads")
+        },
+        filename: function (req, file, cb) {
+            cb(null, file.fieldname + "-" + Date.now() + ".jpg")
+        }
+    })
+}).single('image');
+
 router.get('/tour-itinerary/get', async (req, res) => {
     try {
         const result = await getAllIntineraryTour();
@@ -39,90 +50,72 @@ router.get('/tour-itinerary/get/id/:id', async (req, res) => {
     }
 });
 
-router.post('/tour-itinerary/create-itinerary', async (req, res) => {
+router.post('/tour-itinerary/create-itinerary', upload, async (req, res) => {
     const reqdata = req?.body;
-    console.log(reqdata)
+    console.log(reqdata);
+
+    let activityIndex = reqdata?.activityIndex
+    let tour_id = reqdata?.tour_id
+    let title = reqdata?.title
+    let day = reqdata?.day
+    let description = reqdata?.description
+    let myActivity = reqdata?.myActivity
+    let stay = reqdata?.stay
+    let food = reqdata?.food
+    let status = reqdata?.status
+    let places_to_visit = reqdata?.places_to_visit
+    let travel = reqdata?.travel
+    let image = reqdata?.image
+    let overview = reqdata?.overview
+    let travel_options = reqdata?.travel_options
 
     try {
-        if (reqdata?.length > 0) {
-            reqdata?.map(async (days) => {
+        // await createItineraryValidation.validateAsync({ ...req?.body});
+        const result = await createitinerary([
+            tour_id,
+            title,
+            day,
+            description,
+            myActivity,
+            stay,
+            food,
+            status,
+            places_to_visit,
+            travel,
+            image,
+            overview,
+            travel_options,
+            activityIndex
+        ]);
 
-                days?.activities?.map(async (activity, index) => {
-                    let activityIndex = index
-                    let tour_id = days.tourId
-                    let title = ""
-                    let day = days?.day + 1 // To run calculations smoothly on FE, Adding 1 here...
-                    let description = activity?.description
-                    let myActivity = activity?.activity
-                    let stay = activity?.stay
-                    let food = activity?.food
-                    let status = ""
-                    let places_to_visit = activity?.place
-                    let travel = activity?.travelOption
-                    let image = activity?.image
-                    let overview = activity?.description
-                    let travel_options = activity?.travelOption
-
-                    try {
-
-                        // await createItineraryValidation.validateAsync({ ...req?.body});
-                        const result = await createitinerary([
-                            tour_id,
-                            title,
-                            day,
-                            description,
-                            myActivity,
-                            stay,
-                            food,
-                            status,
-                            places_to_visit,
-                            travel,
-                            image,
-                            overview,
-                            travel_options,
-                            activityIndex
-                        ]);
-
-                        if (result?.length > 0) {
-                            res.status(200).send(
-                                JSON.stringify({
-                                    message: "Itinerary created successfully",
-                                    data: result
-                                })
-                            );
-                        } else {
-                            res.status(500).send(
-                                JSON.stringify({
-                                    message: "Something went wrong",
-                                    error: result
-                                })
-                            )
-                        }
-
-                    } catch (error) {
-                        res.status(404).json({
-                            error: error,
-                            status: "fail",
-                            statusCode: 404
-                        })
-                    }
-
+        if (result?.length > 0) {
+            res.status(200).send(
+                JSON.stringify({
+                    message: "Itinerary created successfully",
+                    data: result
                 })
-            })
+            );
         } else {
-            res.status(200).json({
-                message: "No data added",
-                status: "success",
-                statusCode: 200
-            });
+            res.status(500).send(
+                JSON.stringify({
+                    message: "Something went wrong",
+                    error: result,
+                    statusCode: 200,
+                    status: "fail"
+                })
+            )
         }
     } catch (error) {
-        res.status(404).json({
-            error: error,
-            status: "fail",
-            statusCode: 404
-        })
+        res.status(500).send(
+            JSON.stringify({
+                message: "Something went wrong",
+                error: error,
+                statusCode: 400,
+                status: "fail"
+            })
+        )
     }
+
 });
 
 router.put('/itinerary/update/id/:id', async (req, res) => {
